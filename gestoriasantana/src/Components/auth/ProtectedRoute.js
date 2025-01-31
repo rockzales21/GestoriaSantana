5df981b89d8c6f8c2ccf573374378f77273d6023
OscarR <oscarrosalesgomez@gmail.com>
2025-01-30 18:34:24 -0600

    Se añadio la funcionalidad de login, redireccionando al login si es que no hay una sesion iniciada

--- gestoriasantana/src/Components/auth/ProtectedRoute.js
+++ gestoriasantana/src/Components/auth/ProtectedRoute.js
@@ -1,3 +1,14 @@
+// import { Navigate, Outlet } from "react-router-dom";
+
+// const ProtectedRoute = () => {
+//   const token = localStorage.getItem("token");
+
+//   return token ? <Outlet /> : <Navigate to="/login" />;
+// };
+
+// export default ProtectedRoute;
+
+
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
